13de56aefffda6776db029f6b2f7cca74db3605c
aubrey-heim <aubrey.l.heim@gmail.com>
2021-02-06 17:35:19 -0800

    basic dashboard

---
+++ client/src/utils/fakeBooks.js
@@ -0,0 +1,20 @@
+const FakeBooks = [
+    {
+        id: 1,
+        title: "2021",
+        description: "My current bullet journal"
+    },
+    {
+        id: 2,
+        title: "2020",
+        description: "Journal for the worst year ever"
+    },
+    {
+        id: 3,
+        title: "Family",
+        description: "Lists and activities for the family"
+    }
+];
+
+export default FakeBooks;
+
