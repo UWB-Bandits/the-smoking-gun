2322c06d34e900a36713fec5eb08e54b9a577711
BrandonVA <codebva@gmail.com>
2021-02-08 18:49:57 -0800

    Fixed lint errors.

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,5 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { Box, Grid, Accordion, AccordionSummary, Typography, AccordionDetails, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  Box,
+  Accordion,
+  AccordionSummary,
+  Typography,
+  AccordionDetails,
+} from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import FakeBooks from "../utils/fakeBooks";
 import TitleItem from "../components/TitleItem";
@@ -7,16 +13,16 @@ import { makeStyles } from "@material-ui/core/styles";
 import NewListForm from "../components/NewItemForm";
 
 function IndexPage() {
-  const [formData, setFormData] = useState({ newList: ""});
-
-  //GET ID OFF OF ROUTE AND SET UP STATE FOR THE BOOK SHOWN -- UPDATE REFERENCES TO FakeBooks[0] 
+  const [formData, setFormData] = useState({ newList: "" });
+  console.log(formData);
+  //GET ID OFF OF ROUTE AND SET UP STATE FOR THE BOOK SHOWN -- UPDATE REFERENCES TO FakeBooks[0]
 
   const handleInputChange = (e) => {
     const { value } = e.target;
     setFormData({ newList: value });
   };
 
-  const addList = () =>{
+  const addList = () => {
     //HAVE NEWLIST POST TO DATABASE AND REDIRECT TO "/lists/:listid"
   };
 
@@ -24,12 +30,12 @@ function IndexPage() {
     root: {
       width: "100%",
       marginLeft: "10px",
-      marginRight: "10px"
+      marginRight: "10px",
     },
     accordion: {
       width: "100%",
       marginLeft: "10px",
-      marginRight: "10px"
+      marginRight: "10px",
     },
     heading: {
       fontSize: theme.typography.pxToRem(15),
@@ -38,48 +44,59 @@ function IndexPage() {
   }));
 
   return (
-    <div> 
-        <Box>
-            <TitleItem {...FakeBooks[0]}/>
+    <div>
+      <Box>
+        <TitleItem {...FakeBooks[0]} />
 
-              <Accordion >
-                <AccordionSummary 
-                  className={classes.accordion}
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography className={classes.heading}>Lists</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <ul>
-                      {FakeBooks[0].lists.map(item => <li key={item.name}><a href={`/lists/${item.id}`}>{item.name}</a></li>)}
-                        <NewListForm handleInputChange={handleInputChange} addItem={addList} type="list"/>
-                    </ul>
-                </AccordionDetails>
-              </Accordion>                            
-              <Accordion disabled>
-                <AccordionSummary
-                  className={classes.accordion}
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel3a-content"
-                  id="panel3a-header"
-                >
-                  <Typography className={classes.heading}>Calendars (Feature coming soon!)</Typography>
-                </AccordionSummary>
-              </Accordion>
-              <Accordion disabled>
-                <AccordionSummary
-                  className={classes.accordion}
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel3a-content"
-                  id="panel3a-header"
-                >
-                  <Typography className={classes.heading}>Habits (Feature coming soon!)</Typography>
-                </AccordionSummary>
-              </Accordion>
-        </Box>
-             
+        <Accordion>
+          <AccordionSummary
+            className={classes.accordion}
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.heading}>Lists</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <ul>
+              {FakeBooks[0].lists.map((item) => (
+                <li key={item.name}>
+                  <a href={`/lists/${item.id}`}>{item.name}</a>
+                </li>
+              ))}
+              <NewListForm
+                handleInputChange={handleInputChange}
+                addItem={addList}
+                type="list"
+              />
+            </ul>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion disabled>
+          <AccordionSummary
+            className={classes.accordion}
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content"
+            id="panel3a-header"
+          >
+            <Typography className={classes.heading}>
+              Calendars (Feature coming soon!)
+            </Typography>
+          </AccordionSummary>
+        </Accordion>
+        <Accordion disabled>
+          <AccordionSummary
+            className={classes.accordion}
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel3a-content"
+            id="panel3a-header"
+          >
+            <Typography className={classes.heading}>
+              Habits (Feature coming soon!)
+            </Typography>
+          </AccordionSummary>
+        </Accordion>
+      </Box>
     </div>
   );
 }
--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,39 +1,57 @@
 const db = require("../models");
 
+// phonesRouter.post("/", (req, res) => {
+//   const auth = req.currentUser;
+//   if (auth) {
+//     console.log("authenticated!", auth);
+//     return res.send("Hi, from within the phones router POST");
+//   }
+//   return res.status(403).send("Not authorized");
+// });
+
 // Defining methods for the booksController
 module.exports = {
-  findAll: function(req, res) {
-    db.Book
-      .find(req.query)
+  test: (req, res) => {
+    const auth = req.currentUser;
+    if (auth) {
+      console.log("authenticated!", auth);
+      db.Book.find(req.query)
+        .populate("List")
+        .sort({ date: -1 })
+        .then((dbModel) => res.json(dbModel))
+        .catch((err) => res.status(422).json(err));
+
+      // return res.send("Hi, from within the phones router POST");
+    }
+    return res.status(403).send("Not authorized");
+  },
+  findAll: function (req, res) {
+    db.Book.find(req.query)
       .populate("List")
       .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  findById: function(req, res) {
-    db.Book
-      .populate("List")
+  findById: function (req, res) {
+    db.Book.populate("List")
       .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  create: function(req, res) {
-    db.Book
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  create: function (req, res) {
+    db.Book.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  update: function(req, res) {
-    db.Book
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  update: function (req, res) {
+    db.Book.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
-  remove: function(req, res) {
-    db.Book
-      .findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
+  remove: function (req, res) {
+    db.Book.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
