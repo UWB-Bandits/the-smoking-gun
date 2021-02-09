5dd85f9cdc4a1218dab7832f1928fc648de9accf
aubrey-heim <aubrey.l.heim@gmail.com>
2021-02-08 22:15:23 -0800

    styling

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -9,8 +9,7 @@ function Dashboard() {
   return (
     <div> 
         <Box>
-            <Jumbotron>
-            </Jumbotron>
+            <Jumbotron />
             <Grid container >
                 <BookButton title="Create a new book!" description="Click here to start a new journal" link="/create-book" colorScheme="yellow"/>
                 {FakeBooks.map(item => <BookButton key={item.id} link={`/books/${item.id}`} {...item}/> )}
