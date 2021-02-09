62a8a976cdb3fc84fb30d5e06598243968556293
BrandonVA <codebva@gmail.com>
2021-02-09 14:08:57 -0800

    set up router and commented out unused code.

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/testSmokingGun"
+);
+
+//  ____________Commented out because maybe we don't need it same code as in routes/index.js?
+// const path = require("path");
+
+// // Send every request to the React app
+// // Define any API routes before this runs
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
