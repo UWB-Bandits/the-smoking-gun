fbffae26fde5ea89d32063e8863dab9bb778788e
BrandonVA <codebva@gmail.com>
2021-02-08 18:55:12 -0800

    returned to normal

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,30 +1,7 @@
 const db = require("../models");
 
-// phonesRouter.post("/", (req, res) => {
-//   const auth = req.currentUser;
-//   if (auth) {
-//     console.log("authenticated!", auth);
-//     return res.send("Hi, from within the phones router POST");
-//   }
-//   return res.status(403).send("Not authorized");
-// });
-
 // Defining methods for the booksController
 module.exports = {
-  test: (req, res) => {
-    const auth = req.currentUser;
-    if (auth) {
-      console.log("authenticated!", auth);
-      db.Book.find(req.query)
-        .populate("List")
-        .sort({ date: -1 })
-        .then((dbModel) => res.json(dbModel))
-        .catch((err) => res.status(422).json(err));
-
-      // return res.send("Hi, from within the phones router POST");
-    }
-    return res.status(403).send("Not authorized");
-  },
   findAll: function (req, res) {
     db.Book.find(req.query)
       .populate("List")
