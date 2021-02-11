911f535d67fd354449ed51ca513542a0ac1b0c64
aubrey-heim <aubrey.l.heim@gmail.com>
2021-02-10 17:01:44 -0800

    connected dashboard to backend

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Grid } from "@material-ui/core";
 import Jumbotron from "../components/Jumbotron";
 import BookButton from "../components/BookButton";
-import FakeBooks from "../utils/fakeBooks";
+import API from "../utils/API";
+
+
+
 
 function Dashboard() {
+
+  const [books, setBooks] = useState([]);
+  
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  // Loads all books and sets them to books
+  function loadBooks() {
+    API.getBooks()
+      .then(res => 
+        setBooks(res.data)
+      )
+      .catch(err => console.log(err));
+  }
+
+
   return (
     <div> 
         <Box>
             <Jumbotron />
             <Grid container >
                 <BookButton title="Create a new book!" description="Click here to start a new journal" link="/create-book" colorScheme="yellow"/>
-                {FakeBooks.map(item => <BookButton key={item.id} link={`/books/${item.id}`} {...item}/> )}
+                {books.map(item => <BookButton key={item._id} link={`/books/${item._id}`} {...item}/> )}
                                     
             </Grid>
        
