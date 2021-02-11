8c69111fc058accf7b1529c7f2794f9cd189ef5f
BrandonVA <codebva@gmail.com>
2021-02-10 16:47:07 -0800

    Added more feilds for a user when they sign in

--- client/src/components/SignUpForm/SignUpForm.js
+++ client/src/components/SignUpForm/SignUpForm.js
@@ -10,10 +10,20 @@ import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import PropTypes from "prop-types";
 import FormButtons from "../FormButtons/FormButtons";
 import Box from "@material-ui/core/Box";
-// import fire from "../../utils/firebase";
-
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormLabel from "@material-ui/core/FormLabel";
+import Alert from "@material-ui/lab/Alert";
 const SignInForm = (props) => {
-  const { handleInputChange, setPage, handleSubmit } = props;
+  const {
+    handleInputChange,
+    setPage,
+    handleSubmit,
+    loading,
+    error,
+    formData,
+  } = props;
   const [values, setValues] = useState({
     confirmPassword: "",
     showConfirmPassword: false,
@@ -35,31 +45,11 @@ const SignInForm = (props) => {
       showConfirmPassword: !values.showConfirmPassword,
     });
   };
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log(formData);
-  //   test.createUserWithEmailAndPassWord(formData.email, formData.password);
-  // };
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log(`submitted email:
-  //         ${formData.email} password: ${formData.password} confirm: ${formData.confirmPassword}`);
-
-  //   if (formData.password === formData.confirmPassword) {
-  //     fire
-  //       .auth()
-  //       .createUserWithEmailAndPassword(formData.email, formData.password);
-  //     // console.log("they Match");
-  //   } else {
-  //     console.log("passwords don't match");
-  //   }
-  // };
 
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
-  // console.log(formData);
-
+  console.log(formData);
   return (
     <Box
       boxShadow={2}
@@ -83,67 +73,135 @@ const SignInForm = (props) => {
           justifyContent: "center",
         }}
       >
+        {error && <Alert severity="error">{error}</Alert>}
         <div style={{ margin: "0px 5px" }}>
           <TextField
             style={{ width: "100%" }}
-            id="standard-password-input"
+            id="email-input"
             label="Email"
             type="email"
             name="email"
-            autoComplete="current-password"
+            // autoComplete="current-password"
+            onChange={handleInputChange}
+          />
+          <FormControl style={{ width: "100%" }}>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              id="password"
+              type={values.showPassword ? "text" : "password"}
+              value={values.password}
+              name="password"
+              onChange={handleChange("password")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <FormControl style={{ width: "100%" }}>
+            <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
+            <Input
+              id="confirmPassword"
+              type={values.showConfirmPassword ? "text" : "password"}
+              value={values.confirmPassword}
+              name="confirmPassword"
+              onChange={handleChange("confirmPassword")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowConfirmPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {values.showConfirmPassword ? (
+                      <Visibility />
+                    ) : (
+                      <VisibilityOff />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </div>
+        {/* _______________________first name______________________________________________ */}
+        <div style={{ margin: "0px 5px" }}>
+          <TextField
+            style={{ width: "100%" }}
+            id="first-name-input"
+            label="First Name"
+            type="firstName"
+            name="firstName"
+            // autoComplete="current-password"
             onChange={handleInputChange}
           />
         </div>
-        <FormControl style={{ margin: "0px 5px" }}>
-          <InputLabel htmlFor="password">Password</InputLabel>
-          <Input
-            id="password"
-            type={values.showPassword ? "text" : "password"}
-            value={values.password}
-            name="password"
-            onChange={handleChange("password")}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
+        {/* _________________________ last name ____________________________________________ */}
+        <div style={{ margin: "0px 5px" }}>
+          <TextField
+            style={{ width: "100%" }}
+            id="last-name-input"
+            label="Last Name"
+            type="lastName"
+            name="lastName"
+            // autoComplete="current-password"
+            onChange={handleInputChange}
           />
-        </FormControl>
-        <FormControl style={{ margin: "0px 5px" }}>
-          <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
-          <Input
-            id="confirmPassword"
-            type={values.showConfirmPassword ? "text" : "password"}
-            value={values.confirmPassword}
-            name="confirmPassword"
-            onChange={handleChange("confirmPassword")}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowConfirmPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {values.showConfirmPassword ? (
-                    <Visibility />
-                  ) : (
-                    <VisibilityOff />
-                  )}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
+        </div>
+        {/* ________________________ profile pic_____________________________________________ */}
+        <div style={{ margin: "10px 0px" }}>
+          <FormControl component="fieldset">
+            <FormLabel style={{ margin: "10px 0px" }} component="legend">
+              Choose Your Avatar.
+            </FormLabel>
+            <RadioGroup
+              row
+              defaultValue="image one"
+              aria-label="profile-pic"
+              name="avatar"
+              // value={FormData.avatar}
+              onChange={handleInputChange}
+            >
+              <FormControlLabel
+                labelPlacement="top"
+                value={"image one"}
+                control={<Radio />}
+                label="Image One"
+              />
+              <FormControlLabel
+                labelPlacement="top"
+                value="image two"
+                control={<Radio />}
+                label="Image Two"
+              />
+              <FormControlLabel
+                labelPlacement="top"
+                value="image three "
+                control={<Radio />}
+                label="Image Three"
+              />
+              <FormControlLabel
+                labelPlacement="top"
+                value="image four"
+                // disabled
+                control={<Radio />}
+                label="Image four"
+              />
+            </RadioGroup>
+          </FormControl>
+        </div>
         <FormButtons
           button1="Sign Up"
           button2={["Have an account? ", "Log In"]}
           setPage={setPage}
+          loading={loading}
         />
       </form>
     </Box>
@@ -153,8 +211,10 @@ const SignInForm = (props) => {
 SignInForm.propTypes = {
   setPage: PropTypes.func,
   handleInputChange: PropTypes.func,
-  // formData: PropTypes.object,
+  loading: PropTypes.bool,
   handleSubmit: PropTypes.func,
+  error: PropTypes.string,
+  formData: PropTypes.object,
 };
 
 export default SignInForm;
