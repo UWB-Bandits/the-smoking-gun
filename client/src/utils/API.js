1d7738181838fd67dbf917a6e5d9a8ffb280c3f5
aubrey-heim <aubrey.l.heim@gmail.com>
2021-02-10 22:41:29 -0800

    added API routes for lists

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -17,4 +17,22 @@ export default {
   saveBook: function (bookData) {
     return axios.post("/api/books", bookData);
   },
+  getLists: function () {
+    return axios.get("/api/lists");
+  },
+  // Gets the list with the given id
+  getList: function (id) {
+    return axios.get("/api/lists/" + id);
+  },
+  // Deletes the list with the given id
+  deleteList: function (id) {
+    return axios.delete("/api/lists/" + id);
+  },
+  // Saves a list to the database
+  saveList: function (listData) {
+    return axios.post("/api/lists", listData);
+  },
+  updateList: function (id, listData) {
+    return axios.put("/api/lists/" + id, listData);
+  }
 };
