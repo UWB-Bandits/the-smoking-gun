b165b5e4c73724373d10ef83df6889c099c131b0
Kenneth <kennethkopelson@gmail.com>
2021-02-14 11:46:19 -0800

    added gravitar setup to sign up page

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -11,7 +11,7 @@ const SignIn = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-    avatar: "image one",
+    avatar: "",
   });
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
