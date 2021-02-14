d09063d7462c141acd3c38d9ba40b9117e58de1e
aubrey-heim <aubrey.l.heim@gmail.com>
2021-02-13 20:40:34 -0800

    breadcrumbs added

--- controllers/listsController.js
+++ controllers/listsController.js
@@ -12,6 +12,7 @@ module.exports = {
   findById: function(req, res) {
     db.List
       .findById(req.params.id)
+      .populate("book")
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
