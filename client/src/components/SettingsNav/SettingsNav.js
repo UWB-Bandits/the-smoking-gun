eae144939c04b63a9b835e6cf3ae9e59ba028887
BrandonVA <codebva@gmail.com>
2021-02-15 13:53:17 -0800

    moving styles to a css file.

--- client/src/components/SettingsNav/SettingsNav.js
+++ client/src/components/SettingsNav/SettingsNav.js
@@ -1,34 +1,35 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import PropTypes from "prop-types";
+import "./settings-nav.css";
 
 const SettingsNav = (props) => {
   const { changeDisplay } = props;
-  const styles = {
-    menuContainer: {
-      display: "flex",
-      justifyContent: "space-between",
-      margin: ".5rem 0",
-      background: "#232323",
-      color: "#E0E0E0",
-      textAlign: "center",
-    },
-    menuItems: {
-      padding: ".5rem 3rem",
-    },
-  };
+  // const styles = {
+  //   menuContainer: {
+  //     display: "flex",
+  //     justifyContent: "space-between",
+  //     margin: ".5rem 0",
+  //     background: "#232323",
+  //     color: "#E0E0E0",
+  //     textAlign: "center",
+  //   },
+  //   menuItems: {
+  //     padding: ".5rem 3rem",
+  //   },
+  // };
   return (
-    <Grid item xs={12} style={styles.menuContainer}>
-      <h3 style={styles.menuItems} onClick={changeDisplay}>
+    <Grid item xs={12} id="menuContainer">
+      <h3 className="menuItems" onClick={changeDisplay}>
         Update Profile
       </h3>
-      <h3 style={styles.menuItems} onClick={changeDisplay}>
+      <h3 className="menuItems" onClick={changeDisplay}>
         Change Password
       </h3>
-      <h3 style={styles.menuItems} onClick={changeDisplay}>
+      <h3 className="menuItems" onClick={changeDisplay}>
         Change Avatar
       </h3>
-      <h3 style={styles.menuItems} onClick={changeDisplay}>
+      <h3 className="menuItems" onClick={changeDisplay}>
         Sign Out
       </h3>
     </Grid>
