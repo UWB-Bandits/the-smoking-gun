da53f51ae6d521da3aaa1a14a34658c8eb98d58b
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-17 19:31:26 -0800

    began dashboardAPI on the front end

---
+++ client/src/utils/dashboardAPI.js
@@ -0,0 +1,7 @@
+// import axios from "axios";
+
+// export default {
+//     weatherAPI: function(query) {
+//         return axios.get("http://api.weatherapi.com/v1/current.json?key=" + process.env.REACT_APP_WEATHER_API_KEY + "&q=" + query);
+//     }
+// };
