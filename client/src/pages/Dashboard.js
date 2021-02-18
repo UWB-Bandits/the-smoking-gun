338f04034f067bca09905f3914c191f68539e83c
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-17 19:34:42 -0800

    added weather widget and loading state check

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,25 +5,31 @@ import BookButton from "../components/BookButton";
 import { useAuth } from "../contexts/AuthContext";
 import API from "../utils/API";
 
+import WeatherWidget from "../components/DashboardWidgets/WeatherWidget";
+// import DashboardAPI from "../../../routes/api/dashboardAPI";
+// import DashboardAPI from "../utils/dashboardAPI";
+
 function Dashboard() {
   // Gets current user data
   // Set state based off user id form mongo db
   const [user, setUser] = useState({});
   const [usersBooks, setUsersBooks] = useState([]);
   const { currentUser } = useAuth();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
 
   useEffect(() => {
     getUser();
     getAllBooks();
+    weatherSearch();
   }, []);
 
   const getAllBooks = () => {
-      //  for the currentUser.uid
-
+    //  for the currentUser.uid
     API.getBooksWhere(currentUser.uid)
       .then((res) => {
         setUsersBooks(res.data);
-        console.log(res);
+        // console.log(res);
       })
       .catch((err) => console.log(err));
   };
@@ -32,7 +38,22 @@ function Dashboard() {
     API.getUser(currentUser.uid)
       .then(res => {
         setUser(res.data);
+        // console.log(res);
+      });
+  };
+
+  let query = "90210";
+
+  const weatherSearch = () => {
+    API.getWeather(query)
+      .then(res => {
         console.log(res);
+        setWeather(res.data);
+        setIsLoaded(true);
+      })
+      .catch((err) => {
+        setIsLoaded(true);
+        console.log(err);
       });
   };
 
@@ -50,6 +71,10 @@ function Dashboard() {
           {usersBooks.map((item) => (
             <BookButton key={item._id} link={`/books/${item._id}`} {...item} />
           ))}
+          {isLoaded ?
+            <WeatherWidget weather={weather} />
+             : <div>Loading</div> 
+          }
         </Grid>
       </Box>
     </div>
