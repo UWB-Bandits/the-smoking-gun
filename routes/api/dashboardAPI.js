05eb67ff27670847d38230f1af6d07efe9c310c5
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-18 19:26:25 -0800

    updated api route to post

--- routes/api/dashboardAPI.js
+++ routes/api/dashboardAPI.js
@@ -2,20 +2,14 @@ const axios = require("axios");
 const router = require("express").Router();
 require("dotenv").config();
 
-router.route("/").get((req, res) => {
-    axios.get("http://api.weatherapi.com/v1/current.json?key=" + process.env.REACT_APP_WEATHER_API_KEY + "&q=90210")
-        .then(data => {
-            console.log("test");
-            console.log(data);
-            res.json(data.data);
-        }).catch(err =>
-            res.json(err));
+router.route("/").post((req, res) => {
+    console.log(req.body);
+    axios.get("http://api.weatherapi.com/v1/current.json?key=" + process.env.REACT_APP_WEATHER_API_KEY + "&q=" + req.body.Latitude + "," + req.body.Longitude)
+    .then(data => {
+        console.log(data.data);
+        res.json(data.data);
+    }).catch(err =>
+        res.json(err));
 });
-
-// module.exports = {
-//    const weatherAPI = function(query) {
-//         return axios.get("http://api.weatherapi.com/v1/current.json?key=" + process.env.REACT_APP_WEATHER_API_KEY + "&q=" + query);
-//     };
-// };
 
 module.exports = router;
