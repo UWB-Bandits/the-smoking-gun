f7a4fc8bbd21d4c709d495a3cc107e3df244043d
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-19 19:25:46 -0800

    added newsapi route

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,7 @@ const listRoutes = require("./lists");
 const calendarRoutes = require("./calendars");
 const userRoutes = require("./users");
 const weatherRoute = require("./dashboardAPI");
+const newsRoute = require("./dashboardAPI");
 const habitRoutes = require("./habits");
 
 // Book routes
@@ -14,8 +15,11 @@ router.use("/lists", listRoutes);
 router.use("/calendars", calendarRoutes);
 // user routes
 router.use("/users", userRoutes);
-
+// weatherapi route
 router.use("/weather", weatherRoute);
+// newsapi route
+router.use("/news", newsRoute);
+// habit tracker routes
 router.use("/habits", habitRoutes);
 
 module.exports = router;
