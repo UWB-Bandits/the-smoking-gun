a9df80318eb26b6f4f9bdf845ace3ea4d0687586
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-20 17:22:10 -0800

    working on adding themeprovider context

---
+++ client/src/Colors.js
@@ -0,0 +1,19 @@
+const BookThemes = { 
+    red: {
+        backgroundColor: "red",
+        textColor: "red",
+        fontSize: "100px"
+    },
+    blue: {
+        backgroundColor: "blue",
+        textColor: "blue",
+        fontSize: "100px"
+    },
+    green: {
+        backgroundColor: "green",
+        textColor: "green",
+        fontSize: "100px"
+    }
+};
+
+export default BookThemes;
