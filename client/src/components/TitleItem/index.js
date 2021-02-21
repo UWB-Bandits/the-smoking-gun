31b6e0ed93c7b8f71b7011fba039cdaba8284efb
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-20 17:31:36 -0800

    working on adding themeprovider context

--- client/src/components/TitleItem/index.js
+++ client/src/components/TitleItem/index.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    backgroundColor: "grey",
-    marginBottom: "20px",
-    textAlign: "center"
-  },
-  title: {
-    fontSize: 50,
-    textAlign: "center",
-  },
-});
+import ThemeContext from "../../contexts/ThemeContext";
+import BookThemes from "../../Colors";
+
+// const useStyles = makeStyles({
+//   root: {
+//     minWidth: 275,
+//     backgroundColor: bookTheme.backgroundColor,
+//     marginBottom: "20px",
+//     textAlign: "center"
+//   },
+//   title: {
+//     fontSize: 50,
+//     textAlign: "center",
+//   },
+// });
 
 TitleItem.propTypes = {
   title: PropTypes.string,
@@ -24,10 +27,28 @@ TitleItem.propTypes = {
 };
 
 export default function TitleItem(props) {
+  const bookTheme = useContext(ThemeContext);
+  const currentTheme = BookThemes[bookTheme];
+  // console.log(bookTheme);
+
+  const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+      // backgroundColor: bookTheme.blue.backgroundColor,
+      marginBottom: "20px",
+      textAlign: "center"
+    },
+    title: {
+      fontSize: 50,
+      textAlign: "center",
+    },
+  });
+
   const classes = useStyles();
 
+
   return (
-    <Card className={classes.root} variant="outlined">
+    <Card className={classes.root} variant="outlined" style={{backgroundColor: `${currentTheme.backgroundColor}`}}>
       <CardContent>
         <Typography className={classes.title} gutterBottom>
           {props.title}
