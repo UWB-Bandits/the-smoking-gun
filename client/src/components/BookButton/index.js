12580f289c0c3b10507254ac5d5054830d1de037
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-21 22:43:57 -0800

    removed excess spacing

--- client/src/components/BookButton/index.js
+++ client/src/components/BookButton/index.js
@@ -6,11 +6,6 @@ import BookLogo from "../../utils/images/book-logo.png";
 import EditModal from "../EditModal";
 import DeleteModal from "../DeleteModal";
 
-
-
-
-
-
 const useStyles = makeStyles({
   root:{
     marginRight: "auto",
@@ -21,7 +16,6 @@ const useStyles = makeStyles({
   },
   red: {
     backgroundColor: "#F07C5F",
-
   },
   blue: {
     backgroundColor: "#48C1F0"
@@ -42,7 +36,7 @@ const useStyles = makeStyles({
 export default function BookButton(props) {
   const classes = useStyles();
 
-  const {bookSize} = props;
+  const { bookSize } = props;
 
   BookButton.propTypes = {
     title: PropTypes.string.isRequired,
@@ -54,58 +48,55 @@ export default function BookButton(props) {
     bookSize: PropTypes.object
   };
 
-  if(props.edit){
+  if (props.edit) {
     return(
       <Grid item md={4} xs={6}>
-        
-    <Link href={props.link}>
-        <Card className={classes.root +" " + classes[props.colorScheme]} style={{width:bookSize.bookWidth, height:bookSize.bookWidth}}>
-        
-        <CardActionArea>
-            <CardContent>
-            <Typography className={classes.text} style={{marginTop:bookSize.pushTop, fontSize:bookSize.textSize}} gutterBottom variant="h5" component="h2">
-                {props.title}
-            </Typography>
-            <Typography className={classes.text} style={{fontSize:bookSize.textSize}} variant="body2" color="textSecondary" component="p">
-                {props.description}
-            </Typography>
-            </CardContent>
-        </CardActionArea>
-        
-        </Card>
-    </Link>
-    <Grid container justify="center" >
-          <EditModal 
+        <Link href={props.link}>
+          <Card className={classes.root + " " + classes[props.colorScheme]} style={{width:bookSize.bookWidth, height:bookSize.bookWidth}}>
+            <CardActionArea>
+              <CardContent>
+                <Typography className={classes.text} style={{marginTop:bookSize.pushTop, fontSize:bookSize.textSize}} gutterBottom variant="h5" component="h2">
+                    {props.title}
+                </Typography>
+                <Typography className={classes.text} style={{fontSize:bookSize.textSize}} variant="body2" color="textSecondary" component="p">
+                    {props.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Link>
+      <Grid container justify="center" >
+        <EditModal
           title={props.title}
           description={props.description}
           colorScheme={props.colorScheme}
           id={props.id}
-          />
-          <DeleteModal
+        />
+        <DeleteModal
           title={props.title}
           id={props.id}
-          />
-        </Grid>
+        />
+      </Grid>
     </Grid>
     );
   }
 
   return (
     <Grid item md={4} xs={6}>
-    <Link href={props.link}>
+      <Link href={props.link}>
         <Card className={classes.root +" " + classes[props.colorScheme]} style={{width:bookSize.bookWidth, height:bookSize.bookWidth}}>
-        <CardActionArea>
+          <CardActionArea>
             <CardContent>
-            <Typography className={classes.text + " " + classes.titleText} style={{marginTop:bookSize.pushTop, fontSize:bookSize.textSize}} gutterBottom variant="h5" component="h2">
-                {props.title}
-            </Typography>
-            <Typography className={classes.text} style={{fontSize:bookSize.textSize}} variant="body2" color="textSecondary" component="p">
-                {props.description}
-            </Typography>
+              <Typography className={classes.text + " " + classes.titleText} style={{marginTop:bookSize.pushTop, fontSize:bookSize.textSize}} gutterBottom variant="h5" component="h2">
+                  {props.title}
+              </Typography>
+              <Typography className={classes.text} style={{fontSize:bookSize.textSize}} variant="body2" color="textSecondary" component="p">
+                  {props.description}
+              </Typography>
             </CardContent>
-        </CardActionArea>
+          </CardActionArea>
         </Card>
-    </Link>
+      </Link>
     </Grid>
   );
 }
