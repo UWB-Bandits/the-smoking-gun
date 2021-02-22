a7c7927e88dd12e1954909370433e5a0c31f1df7
aubrey-heim <aubrey.l.heim@gmail.com>
2021-02-21 16:54:47 -0800

    made calendar prompt modal

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -5,21 +5,28 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import TemporaryDrawer from "../TemporaryDrawer";
 import PropTypes from "prop-types";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import {CircularProgress, Grid} from "@material-ui/core/";
 import API from "../../utils/API";
 import { useParams } from "react-router-dom";
+import PromptModal from "../promptModal";
 
 let eventGuid = 0;
 
 export default function Calendar(props) {
     const [weekendsVisible, setWeekendsVisible] = useState(false);
     const [currentEvents, setCurrentEvents] = useState([]);
+    const [formData, setFormData] = useState("");
     const {id} = useParams();
+    const [titleOpen, setTitleOpen] = useState(false);
 
     useEffect(() => {
         setCurrentEvents(props.calendar.events);
         loadCurrentEvents();
     }, []);
+  
+    const handleClose = () => {
+      setTitleOpen(false);
+    };
 
     function loadCurrentEvents(){
       API.getCalendar(id)
@@ -29,9 +36,13 @@ export default function Calendar(props) {
       .catch(err => console.log(err));
     }
     
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    };
     
     function handleWeekendsToggle(){
-          setWeekendsVisible(!weekendsVisible);
+      setWeekendsVisible(!weekendsVisible);
     }
 
     function handleEventClick(clickInfo) {
@@ -41,7 +52,7 @@ export default function Calendar(props) {
     }
 
     function handleEvents(events) {  
-        setCurrentEvents(events);
+      setCurrentEvents(events);
     }
 
     function addEvent(){
@@ -74,22 +85,25 @@ export default function Calendar(props) {
         return String(eventGuid++);
     }
 
-    function handleDateSelect(selectInfo) {
-        let title = prompt("Please enter a new title for your event");
-        let calendarApi = selectInfo.view.calendar;
-    
-        calendarApi.unselect(); // clear date selection
-    
-        if (title) {
-          calendarApi.addEvent({
-            id: createEventId(),
-            title,
-            start: selectInfo.startStr,
-            end: selectInfo.endStr,
-            allDay: selectInfo.allDay
-          });
-        }
+    function handleDateSelect() {
+      setTitleOpen(true);
     }
+
+    const onTitleSubmit = (selectInfo) =>{
+      let calendarApi = selectInfo.view.calendar;
+      if(formData !== ""){
+        calendarApi.addEvent({
+          id: createEventId(),
+          title: formData,
+          start: selectInfo.startStr,
+          end: selectInfo.endStr,
+          allDay: selectInfo.allDay
+        });
+      }
+      calendarApi.unselect();
+      setTitleOpen(false);
+    };
+
     if(currentEvents){
       return (
         <div>
@@ -121,6 +135,17 @@ export default function Calendar(props) {
               eventChange={addEvent}
               eventRemove={addEvent}
             />
+            <Grid container justify="center" >
+              <PromptModal 
+              prompt="Please enter a new title for your event"
+              handleSubmit = {onTitleSubmit}
+              handleInputChange = {handleInputChange}
+              handleClose = {handleClose}
+              buttonLabel ="Save event"
+              open={titleOpen}
+              />
+            </Grid>
+    
         </div>
       ); 
     } else {
