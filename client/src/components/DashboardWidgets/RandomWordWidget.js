6cce7129d877618dca479971cea2f44802edc237
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-21 22:23:39 -0800

    adjusted styling

--- client/src/components/DashboardWidgets/RandomWordWidget.js
+++ client/src/components/DashboardWidgets/RandomWordWidget.js
@@ -2,12 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    bottom: "60px",
+    maxHeight: "200px",
+    maxWidth: "300px",
+    border: "3px solid #000000",
+    padding: "10px",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    float: "inherit",
   },
   bullet: {
     display: "inline-block",
@@ -15,7 +21,8 @@ const useStyles = makeStyles({
     transform: "scale(0.8)",
   },
   title: {
-    fontSize: 14,
+    fontSize: 22,
+    fontWeight: "bold",
   },
   pos: {
     marginBottom: 12,
@@ -27,20 +34,18 @@ export default function WordCard({ randomWord }) {
 
   return (
     <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          {randomWord[0].word}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          {randomWord[0].definition}
-        </Typography>
-        <Typography variant="body2" component="p">
-          Pronunciation: {randomWord[0].pronunciation}
-        </Typography>
-      </CardContent>
+      <Typography variant="h5" component="h2" gutterbottom>
+        Word of the Day
+      </Typography>
+      <Typography className={classes.title} variant="h5" component="h2" gutterBottom>
+        {randomWord[0].word}
+      </Typography>
+      <Typography className={classes.pos} color="textSecondary">
+        {randomWord[0].definition}
+      </Typography>
+      <Typography variant="body2" component="p">
+        Pronunciation: {randomWord[0].pronunciation}
+      </Typography>
     </Card>
   );
 }
