581f3646a29c479eb6a341d27b29466aa2ed056b
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-02-21 17:23:30 -0800

    added class to retrieve css data

--- client/src/components/TitleItem/index.js
+++ client/src/components/TitleItem/index.js
@@ -1,54 +1,32 @@
-import React, { useContext } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
 
-import ThemeContext from "../../contexts/ThemeContext";
-import BookThemes from "../../Colors";
-
-// const useStyles = makeStyles({
-//   root: {
-//     minWidth: 275,
-//     backgroundColor: bookTheme.backgroundColor,
-//     marginBottom: "20px",
-//     textAlign: "center"
-//   },
-//   title: {
-//     fontSize: 50,
-//     textAlign: "center",
-//   },
-// });
-
 TitleItem.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
 };
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    marginBottom: "20px",
+    textAlign: "center"
+  },
+  title: {
+    fontSize: 50,
+    textAlign: "center",
+  },
+});
+
 export default function TitleItem(props) {
-  const bookTheme = useContext(ThemeContext)[0];
-  const currentTheme = BookThemes[bookTheme];
-  // console.log(bookTheme);
-
-  const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
-      // backgroundColor: bookTheme.blue.backgroundColor,
-      marginBottom: "20px",
-      textAlign: "center"
-    },
-    title: {
-      fontSize: 50,
-      textAlign: "center",
-    },
-  });
-
   const classes = useStyles();
 
-
   return (
-    <Card className={classes.root} variant="outlined" style={{backgroundColor: `${currentTheme.backgroundColor}`}}>
+    <Card className={`${classes.root} background`} variant="outlined">
       <CardContent>
         <Typography className={classes.title} gutterBottom>
           {props.title}
