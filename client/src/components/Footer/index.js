58d16c248640b0b5ad0ef1000e49f960e15fd963
Kenneth <kennethkopelson@gmail.com>
2021-02-25 14:49:46 -0800

    added comments to the Footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,15 @@
+//import react
 import React from "react";
+//import Material-Ui functions
 import { makeStyles } from "@material-ui/core/styles";
+//import Material-Ui components
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+//import Material-Ui icons
 import SettingsIcon from "@material-ui/icons/Settings";
 import HomeIcon from "@material-ui/icons/Home";
 import ImportContactsIcon from "@material-ui/icons/ImportContacts";
-
+//initialize useStyles variable that uses Material-UI's styling solution makeStyles() function
 const useStyles = makeStyles({
   root: {
     textAlign: "center",
@@ -21,14 +25,17 @@ const useStyles = makeStyles({
     width: "100%",
   },
 });
-
+//exports the SimpleBottomNavigation component
 export default function SimpleBottomNavigation() {
+  //initialize the classes variable with our useStyles hook
   const classes = useStyles();
-
+  //this returns footer with icons that send the user to various parts of the webpage
   return (
     <div id="footer">
       <div className={classes.phantom} />
+      {/*Material-UI's BottomNavigation allows movement between primary destinations in an app. */}
       <BottomNavigation showLabels className={classes.root}>
+        {/*Material-UI's BottomNavigationAction is used to wrap the action within the BottomNavigation component */}
         <BottomNavigationAction
           href="/dashboard"
           label="Dashboard"
@@ -40,7 +47,6 @@ export default function SimpleBottomNavigation() {
           icon={<ImportContactsIcon />}
         />
         <BottomNavigationAction
-          // onClick={Settings}
           href="/settings"
           label="Settings"
           icon={<SettingsIcon />}
