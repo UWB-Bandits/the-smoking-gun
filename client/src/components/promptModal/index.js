304ee13d93d1d6b2e1cd714d7f3e2894898b7612
Kenneth <kennethkopelson@gmail.com>
2021-02-25 15:55:05 -0800

    added comments to promptModal

--- client/src/components/promptModal/index.js
+++ client/src/components/promptModal/index.js
@@ -1,25 +1,24 @@
+//import react
 import React from "react";
+//import Material UI function
 import { makeStyles } from "@material-ui/core/styles";
-import {
-  Modal,
-  Box,
-  TextField,
-  Button
-} from "@material-ui/core/";
-import PropTypes from "prop-types";
+//import Material UI components
+import { Modal, Box, TextField, Button } from "@material-ui/core/";
+//import Material UI icon
 import SaveIcon from "@material-ui/icons/Save";
-
+//import a dependency that keeps track of the prop types
+import PropTypes from "prop-types";
+//this function helps position the modal where open
 function getModalStyle() {
   const top = 50;
   const left = 50;
-
   return {
     top: `${top}%`,
     left: `${left}%`,
     transform: `translate(-${top}%, -${left}%)`,
   };
 }
-
+//initialize useStyles variable that uses Material-UI's styling solution makeStyles() function
 const useStyles = makeStyles((theme) => ({
   paper: {
     position: "absolute",
@@ -30,16 +29,15 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2, 4, 3),
   },
 }));
-
+//initialize and export PromptModal component
 export default function PromptModal(props) {
+  //initialize the classes variable with our useStyles hook
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = React.useState(getModalStyle);
-
+  //deconstruct variables from props
   const {handleSubmit, handleInputChange, prompt, open, handleClose, buttonLabel} = props;
-
-
-
+  //sets up prop types for the PromptModal component
   PromptModal.propTypes = {
     prompt: PropTypes.string,
     handleInputChange: PropTypes.func,
@@ -48,9 +46,10 @@ export default function PromptModal(props) {
     handleClose: PropTypes.func,
     buttonLabel: PropTypes.string,
   };
-
+  //this returns a modal with a passed down prompt
   return (
     <div>
+      {/* Material-UI component that provides a solid foundation for creating dialogs, popovers, lightboxes, or whatever else. */}
       <Modal
         open={open}
         onClose={handleClose}
@@ -58,6 +57,7 @@ export default function PromptModal(props) {
         aria-describedby="simple-modal-description"
       >
         <div style={modalStyle} className={classes.paper}>
+          {/* Material-UI component that serves as a wrapper component for most of the CSS utility needs. */}
           <Box
             boxShadow={2}
             p={2}
@@ -79,6 +79,7 @@ export default function PromptModal(props) {
               }}
             >
               <div style={{ margin: "0px 5px" }}>
+                {/* Material-Ui component that serves as a convenience wrapper */}
                 <TextField
                   style={{ width: "100%" }}
                   id="promptAnswer"
@@ -87,6 +88,7 @@ export default function PromptModal(props) {
                   onChange={handleInputChange}
                 />
               </div>
+              {/* Material-UI component that allows users to take actions, and make choices, with a single tap. */}
               <Button
                 style={{
                   margin: "25px 10px 25px auto",
@@ -94,7 +96,7 @@ export default function PromptModal(props) {
                 }}
                 variant="contained"
                 color="primary"
-                startIcon={<SaveIcon />}
+                startIcon={<SaveIcon />}//Material-UI icon component
                 onClick={handleSubmit}
               >
                 {buttonLabel}
@@ -105,5 +107,4 @@ export default function PromptModal(props) {
       </Modal>
     </div>
   );
-  
 }
